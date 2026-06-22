5c636e48b9a90b98e960b82b2594a97423df1cd6
Kennedy <guilhermesamaral9@gmail.com>
2026-06-22 15:43:09 -0300

    estilização posts

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,23 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-route::get('/', [PostController::class, 'index'])->name('post.index');
-route::get('/show-user/{user}', [UserController::class,'show'])->name('user.show');
-route::get('/create-user', [UserController::class, 'create'])->name('user.create');
-route::post('/store-user', [UserController::class, 'store'])->name('user-store');
-route::get('/edit-user/{user}', [UserController::class,'edit'])->name('user.edit');
-route::put('/update-user/{user}', [UserController::class, 'update'])->name('user-update');
-route::delete('/destroy-user/{user}', [UserController::class,'destroy'])->name('user.destroy');
+Route::prefix('user')->group(function () {
+    Route::get('/',                         [UserController::class, 'index'])->name('user.index');
+    Route::get('/show-user/{user}',         [UserController::class,'show'])->name('user.show');
+    Route::get('/create-user',              [UserController::class, 'create'])->name('user.create');
+    Route::post('/store-user',              [UserController::class, 'store'])->name('user-store');
+    Route::get('/edit-user/{user}',         [UserController::class,'edit'])->name('user.edit');
+    Route::put('/update-user/{user}',       [UserController::class, 'update'])->name('user-update');
+    Route::delete('/destroy-user/{user}',   [UserController::class,'destroy'])->name('user.destroy');
+});
 
+Route::prefix('post')->group(function (){
+    Route::get('/', [PostController::class, 'index'])->name('post.index');
+});
 
-// route::get('/profile/')
-route::get('/profile/show-profile/{user}', [ProfileController::class,'show'])->name('profile.show');
-
-
+Route::prefix('profile')->group( function () {
+    Route::get('/profile/show-profile/{user}', [ProfileController::class,'show'])->name('profile.show');
+});
 
 
 /*
